fdc6f8fd42244da435f5eec2f4a23ee0457ab16b
ccQpein <ccqpein@protonmail.com>
2018-10-23 20:20:34 -0400

    a bit

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
+use subprocess::*;
 
+struct Config<'a> {
+    Comm: &'a str,
+}
+
+fn start_new_subprocessing(comm: &str, config: &Config) {
+    let exit_status = subprocess::Exec::cmd("").arg("").join()?;
+}
