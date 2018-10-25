0dbe1447b611dfe6001bb145f7655e8697eeb5c1
ccQpein <ccqpein@protonmail.com>
2018-10-25 19:22:17 -0400

    a bit

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,19 @@
 use std::process::Command;
 
-struct Config<'a> {
+pub struct Config<'a> {
     Comm: &'a str,
+    Args: &'a Vec<&'static str>,
+    Stdout: &'a str,
 }
 
 impl<'a> Config<'a> {
-    fn new() {}
-    fn read_from() {}
+    pub fn new() {}
+    pub fn read_from() {}
 }
 //:= MARK: need find a way to store process id, and handle stdout
-fn start_new_subprocessing(comm: &str, config: &Config) {}
+fn start_new_subprocessing(comm: &str, config: &Config) {
+    let mut child = Command::new(config.Comm).args(config.Args);
+}
 
 //:= MARK: take care all children in case they are stop running
 fn watch_child() {}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+//extern crate supervisor_rs;
+use supervisor_rs::Config;
+
 fn main() {
+    let a = Config::new();
     println!("Hello, world!");
 }
