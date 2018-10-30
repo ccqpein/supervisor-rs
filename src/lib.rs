c15a810b8673f0228f3d3e503699da2d198314cf
ccQpein <ccqpein@protonmail.com>
2018-10-29 22:08:04 -0400

    a bit

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 extern crate yaml_rust;
 
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process::Command;
@@ -21,25 +22,34 @@ impl Config {
         }
     }
 
-    pub fn read_from_str(input: &'static str) -> Result<Self, ScanError> {
-        if let Ok(docs) = YamlLoader::load_from_str(input) {
-            let doc = &docs[0];
-            let comm = doc["Command"][0].as_str().unwrap();
-            return Ok(Config {
-                Comm: comm.to_string(),
-                Args: None,
-                Stdout: String::new(),
-            });
+    pub fn read_from_str(input: &str) -> Result<Self, String> {
+        let temp = YamlLoader::load_from_str(input);
+        match temp {
+            Ok(docs) => {
+                let doc = &docs[0];
+                let comm = doc["Command"][0].clone();
+                return Ok(Config {
+                    Comm: comm.into_string().unwrap(),
+                    Args: None,
+                    Stdout: String::new(),
+                });
+            }
+            Err(e) => return Err(e.description().to_string()),
         }
-
-        return Ok(Config {
-            Comm: String::new(),
-            Args: None,
-            Stdout: String::new(),
-        });
     }
 
-    pub fn read_from_yaml() {}
+    pub fn read_from_yaml_file(filename: &str) -> Result<Self, String> {
+        let contents = File::open(filename);
+        let mut string_result = String::new();
+        match contents {
+            Ok(mut cont) => {
+                cont.read_to_string(&mut string_result);
+                return Self::read_from_str(string_result.as_str());
+            }
+
+            Err(e) => return Err(e.description().to_string()),
+        }
+    }
 }
 //:= MARK: need find a way to store process id, and handle stdout
 pub fn start_new_subprocessing(config: &Config) {
