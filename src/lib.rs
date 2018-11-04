29874e3f3858207c5bb7b5750a43e3f0742b973e
ccQpein <ccqpein@protonmail.com>
2018-11-04 00:33:25 -0400

    a bit

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,59 @@
-extern crate yaml_rust;
-
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process::Command;
-use yaml_rust::{ScanError, YamlEmitter, YamlLoader};
+use yaml_rust::{ScanError, Yaml, YamlEmitter, YamlLoader};
 
 #[derive(Debug)]
 pub struct Config {
-    Comm: String,
-    Args: Option<Vec<String>>,
-    Stdout: String,
+    comm: String,
+    stdout: Option<String>,
+    stderr: Option<String>,
 }
 
 impl Config {
-    pub fn new(comm: String, args: Vec<String>, stdout: String) -> Self {
+    pub fn new(comm: String) -> Self {
         Config {
-            Comm: comm,
-            Args: Some(args),
-            Stdout: stdout,
+            comm: comm,
+            stdout: None,
+            stderr: None,
         }
     }
 
+    pub fn new_stdout(mut self, stdout: String) -> Self {
+        self.stdout = Some(stdout);
+        self
+    }
+
+    pub fn new_stderr(mut self, stderr: String) -> Self {
+        self.stderr = Some(stderr);
+        self
+    }
+
+    //:= TODO: need more generic and match block
     pub fn read_from_str(input: &str) -> Result<Self, String> {
         let temp = YamlLoader::load_from_str(input);
+
+        let mut result: Self;
         match temp {
             Ok(docs) => {
                 let doc = &docs[0];
-                let comm = doc["Command"][0].clone();
-                return Ok(Config {
-                    Comm: comm.into_string().unwrap(),
-                    Args: None,
-                    Stdout: String::new(),
-                });
+
+                result = Self::new(doc["Command"][0].clone().into_string().unwrap());
+
+                if let Some(stdo) = doc["Stdout"][0].clone().into_string() {
+                    result = result.new_stdout(stdo)
+                }
+
+                if let Some(stde) = doc["Stderr"][0].clone().into_string() {
+                    result = result.new_stderr(stde);
+                }
             }
+
             Err(e) => return Err(e.description().to_string()),
         }
+
+        Ok(result)
     }
 
     pub fn read_from_yaml_file(filename: &str) -> Result<Self, String> {
@@ -50,13 +68,35 @@ impl Config {
             Err(e) => return Err(e.description().to_string()),
         }
     }
+
+    fn split_args(&self) -> (String, Option<String>) {
+        let split_comm: Vec<_> = self.comm.splitn(2, ' ').collect();
+
+        if split_comm.len() > 1 {
+            return (split_comm[0].to_string(), Some(split_comm[1].to_string()));
+        }
+
+        (split_comm[0].to_string(), None)
+    }
 }
+
 //:= MARK: need find a way to store process id, and handle stdout
 pub fn start_new_subprocessing(config: &Config) {
-    let mut child = Command::new(&config.Comm)
-        .args(config.Args.as_ref().unwrap())
-        .stdout(File::create(&config.Stdout).unwrap())
-        .spawn();
+    let (com, args) = config.split_args();
+
+    let mut child = Command::new(&com);
+
+    match args {
+        Some(arg) => {
+            child.args(arg.split(' ').collect::<Vec<&str>>());
+        }
+        _ => (),
+    }
+
+    child
+        .stdout(File::create(config.stdout.as_ref().unwrap()).unwrap())
+        .spawn()
+        .expect("Cannot start a new child processing");
 }
 
 //:= MARK: take care all children in case they are stop running
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,9 @@
 use supervisor_rs::*;
 
 fn main() {
-    let bs = "
-Command:
-    - go
-Args:
-    - aa
-";
-    let b = Config::read_from_str(bs).unwrap();
-    println!("{:?}", b);
+    let c = Config::read_from_yaml_file("/tmp/test.yml").unwrap();
+    println!("{:?}", c);
+
+    //start_new_subprocessing(&c);
     loop {}
 }
