6869d98b067978e0d83b94035b0c3336676eccbe
ccQpein <ccqpein@protonmail.com>
2018-12-28 13:41:59 -0500

    tiny bug fix

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -10,7 +10,7 @@ fn main() {
         return;
     }
 
-    let k = if change_2_vec.len() == 1 {
+    let k = if change_2_vec.len() != 1 {
         server::start_new_server(&change_2_vec[1])
     } else {
         server::start_new_server("")
