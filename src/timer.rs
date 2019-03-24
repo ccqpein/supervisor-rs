856ce651dbd02f1e48c868a9f0f789cba710f288
ccQpein <ccqpein@protonmail.com>
2019-03-24 00:10:46 -0400

    start to write timer

--- src/timer.rs
+++ src/timer.rs
@@ -1,1 +1,14 @@
 use super::kindergarten::*;
+use std::io::{Error as ioError, ErrorKind, Read, Result, Write};
+use std::{thread, time};
+
+pub fn timer_fn<F>(interval: u64, func: F) -> Result<String>
+where
+    F: Fn() -> Result<String>,
+{
+    let sleep_time_seconds = time::Duration::from_secs(interval);
+    thread::sleep(sleep_time_seconds);
+    func()
+}
+
+//timer_fn(5,day_care(kg))
