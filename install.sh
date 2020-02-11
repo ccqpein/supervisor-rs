ff082917d91a21aa8784bc82f4230dc80aae7df1
ccQpein <ccqpein@protonmail.com>
2020-02-10 20:31:46 -0500

    delete install script

--- install.sh
+++
@@ -1,9 +0,0 @@
-mkdir /usr/bin/supervisor-rs;
-cd /usr/bin/supervisor-rs/;
-
-if [ $(uname) = "Linux" ]
-then
-    rm ./supervisor-rs-client ./supervisor-rs-server && curl -L https://github.com/ccqpein/supervisor-rs/releases/latest/download/linux.zip | tar zx;
-else
-    rm ./supervisor-rs-client ./supervisor-rs-server && curl -L https://github.com/ccqpein/supervisor-rs/releases/latest/download/macos.zip | tar zx;
-fi
